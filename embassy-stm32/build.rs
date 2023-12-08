eca686089ae2419ff2eb89707fe879d74fce31cf
TheEppicJR <mantacast4154@gmail.com>
2023-12-08 12:02:40 -0800

    It is ok to do this apparently, so removed comment

--- embassy-stm32/build.rs
+++ embassy-stm32/build.rs
@@ -438,7 +438,7 @@ fn main() {
     // ========
     // Generate RccPeripheral impls
 
-    let refcounted_peripherals = HashSet::from(["usart", "adc", "can"]); // wrong way of doing this, looking for kind == "can" will also effect bxcan chips
+    let refcounted_peripherals = HashSet::from(["usart", "adc", "can"]);
     let mut refcount_statics = BTreeSet::new();
 
     for p in METADATA.peripherals {
